6c66abeb30ad85a40e0b6f2368582a86c674819d
Dzulmidnight <yasser.midnight@gmail.com>
2016-07-19 14:36:44 -0500

    actualización conexiones
    Se actualizo el archivo de conexiones

--- connections/kafeprod_bio.php
+++ connections/kafeprod_bio.php
@@ -1,13 +1,13 @@
 <?php
 $hostname_kafeprod_bio = "localhost";
 //$database_kafeprod_bio = "fa000006_dspp";
-/*$username_kafeprod_bio = "root";
+$username_kafeprod_bio = "root";
 $password_kafeprod_bio = "";
-*/
+
 
 $database_kafeprod_bio = "kafeprod_bio";
-$username_kafeprod_bio = "kafeprod_user";
-$password_kafeprod_bio = "SV(miXSO.rph";
+/*$username_kafeprod_bio = "kafeprod_user";
+$password_kafeprod_bio = "SV(miXSO.rph";*/
 $kafeprod_bio = mysql_connect($hostname_kafeprod_bio, $username_kafeprod_bio, $password_kafeprod_bio) or trigger_error(mysql_error(),E_USER_ERROR); 
 
 ?>
