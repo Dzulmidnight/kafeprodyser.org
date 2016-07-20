49f1c733b8a7f9bfb1902ffdcb570567b17ecba0
Dzulmidnight <yasser.midnight@gmail.com>
2016-07-20 00:00:31 -0500

    se crea selector para menu
    Se crea un selector para las opciones del menu

--- system/organizacion/ciclo/selector.php
+++ system/organizacion/ciclo/selector.php
@@ -1,10 +1,20 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>Untitled Document</title>
-</head>
-
-<body>
-</body>
-</html>
+<?php 
+if(isset($_GET['menu'])){
+	$menu = $_GET['menu'];
+}
+switch ($menu) {
+	case 'cuenta':
+		include('cuenta/detalle.php');
+		break;
+	case 'usuarios':
+		include("users/users.php");
+		break;
+	case 'articulo':
+		include("articulo/articulo.php");
+		break;
+	
+	default:
+		include("");
+		break;
+}
+ ?>
