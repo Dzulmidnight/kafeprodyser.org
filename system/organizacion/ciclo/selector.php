ab6d70fcb6f4d8ea3b3a5aa363ef43c75ce65cde
JMH <isc.jesusmartinez@gmail.com>
2016-07-20 14:55:41 -0500

    selector ciclo
    selector ciclo y creación de carpetas

--- system/organizacion/ciclo/selector.php
+++ system/organizacion/ciclo/selector.php
@@ -1,10 +1,19 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>Untitled Document</title>
-</head>
+<?php 
 
-<body>
-</body>
-</html>
+
+
+if(isset($_GET['padron'])){
+	include('padron/index.php');
+}
+else
+if(isset($_GET['actividad'])){
+	include('actividad/index.php');
+}else
+if(isset($_GET['fotografia'])){
+	include('fotografia/index.php');
+}
+	
+	
+	
+
+ ?>
